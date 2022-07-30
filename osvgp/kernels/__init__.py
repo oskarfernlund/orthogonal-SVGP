284ce81124aa6c9104a6965def1615d9aac80940
oskar <oskar.fernlund@outlook.com>
2022-07-30 18:18:22 +0100

    added general kernal class, linear and sqexp kernels

---
+++ osvgp/kernels/__init__.py
@@ -0,0 +1,7 @@
+from gpflow.kernels import Kernel
+from gpflow.kernels import SquaredExponential
+from gpflow.kernels import Linear
+
+__all__ = ["Kernel", 
+           "SquaredExponential", 
+           "Linear"]
