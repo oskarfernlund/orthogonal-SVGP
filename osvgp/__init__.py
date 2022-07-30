5e0a44d24dbf0e5448d79569673c7f946c9fcecf
oskar <oskar.fernlund@outlook.com>
2022-07-30 18:22:34 +0100

    added new subpackages

--- osvgp/__init__.py
+++ osvgp/__init__.py
@@ -1,2 +1,25 @@
-from . import (base,datasets, metrics, models, plotting, util)
-__all__ = ["base", "datasets", "metrics", "models", "plotting", "util"]
+from . import base
+from . import config
+from . import datasets
+from . import experiments
+from . import kernels
+from . import likelihoods
+from . import metrics
+from . import models
+from . import optimisers
+from . import plotting
+from . import processing
+from . import util
+
+__all__ = ["base", 
+           "config",
+           "datasets",
+           "experiments",
+           "kernels",
+           "likelihoods",
+           "metrics",
+           "models",
+           "optimisers",
+           "plotting",
+           "processing",
+           "util"]
