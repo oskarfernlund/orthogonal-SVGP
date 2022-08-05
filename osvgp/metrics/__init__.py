b289a71dc262e57735c5c0146f5ad36d79aba2d3
oskar <oskar.fernlund@outlook.com>
2022-08-05 17:46:49 +0100

    added rmse function

--- osvgp/metrics/__init__.py
+++ osvgp/metrics/__init__.py
@@ -1,6 +1,8 @@
-from .densities import compute_lpd
+from .accuracies import compute_lpd
+from .accuracies import compute_rmse
 from .divergences import compute_kl_from_exact_posterior
 
 __all__ = ["compute_lpd",
+           "compute_rmse",
            "compute_kl_from_exact_posterior"]
            
