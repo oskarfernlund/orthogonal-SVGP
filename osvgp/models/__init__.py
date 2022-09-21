1f0fb8339cbf8d5ab340031a3355e26aa153cdcb
oskar <oskar.fernlund@outlook.com>
2022-09-21 12:51:29 +0100

    added new model class to __init__

--- osvgp/models/__init__.py
+++ osvgp/models/__init__.py
@@ -3,9 +3,11 @@ from gpflow.models import InternalDataTrainingLossMixin
 from .gpr import GPR
 from .sgpr import SGPR
 from .osgpr import OSGPR
+from .experimental import OSGPRBoundGap
 
 __all__ = ["GPModel",
            "InternalDataTrainingLossMixin",
            "GPR",
            "SGPR",
-           "OSGPR"]
+           "OSGPR",
+           "OSGPRBoundGap"]
